afaf4b8a00c3c8f0a7230a280a0b8b02c4cf39a9
Jordan Bonecutter <i@jordanbonecutter.com>
2023-12-02 16:03:39 -0800

    day 1 part 2

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -3,6 +3,17 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Failed reading file");
     let sum: u32 = contents.lines().map(|line| {
+        let mut ret = line.replace("one", "one1one");
+        ret = ret.replace("two", "two2two");
+        ret = ret.replace("three", "three3three");
+        ret = ret.replace("four", "four4four");
+        ret = ret.replace("five", "five5five");
+        ret = ret.replace("six", "six6six");
+        ret = ret.replace("seven", "seven7seven");
+        ret = ret.replace("eight", "eight8eight");
+        ret = ret.replace("nine", "nine9nine");
+        return ret;
+    }).map(|line| {
         if line.len() == 0 {
             return 0;
         }
